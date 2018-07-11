c730fc41c61bd43b2187294b5c78d6e8566463cc
edwin <edwin@edwins-MacBook-Pro.local>
2018-07-11 22:39:13 +0800

    changes

--- ImoocSparkSQLProject/src/main/scala/com/boy/spark/DataFrameApp.scala
+++ ImoocSparkSQLProject/src/main/scala/com/boy/spark/DataFrameApp.scala
@@ -38,5 +38,4 @@ object DataFrameApp {
     spark.close()
 
   }
-
 }
--- ImoocSparkSQLProject/src/main/scala/com/boy/spark/DataFrameCase.scala
+++ ImoocSparkSQLProject/src/main/scala/com/boy/spark/DataFrameCase.scala
@@ -1,5 +1,6 @@
 package com.boy.spark
 
+import com.tool.LogConf
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -8,45 +9,47 @@ import org.apache.spark.sql.SparkSession
 object DataFrameCase {
   def main(args: Array[String]): Unit = {
 
+    LogConf.setStreamingLogLevels
+
     val spark = SparkSession.builder().appName("DataFrameCase").master("local[2]").getOrCreate()
 
     val rdd = spark.sparkContext.textFile("src/main/data/student.data")
 
     import spark.implicits._
-    val studentDF = rdd.map(_.split("\\|")).map(line => Student(line(0).toInt, line(1), line(2), line(3))).toDF()
+    val studentDS = rdd.map(_.split("\\|")).map(line => Student(line(0).toInt, line(1), line(2), line(3))).toDS()
 
-    studentDF.show(30, false)
+    studentDS.show(30, false)
     //前10条
-    studentDF.take(10).foreach(println)
+    studentDS.take(10).foreach(println)
     //第一条记录
-    studentDF.first()
+    studentDS.first()
     //前三条
-    studentDF.head(3)
+    studentDS.head(3)
     //指定的列
-    studentDF.select("name", "email").show(30, false)
+    studentDS.select("name", "email").show(30, false)
     //过滤name=''
-    studentDF.filter("name='' OR name='NULL'").show()
+    studentDS.filter("name='' OR name='NULL'").show()
 
     //name以M开头的人
-    studentDF.filter("SUBSTR(name,0,1)='M'").show()
+    studentDS.filter("SUBSTR(name,0,1)='M'").show()
     //查询内置函数
     spark.sql("show functions").show(1000)
 
     // //name以M开头的人
-    studentDF.filter("substring(name,0,1)='M'").show()
+    studentDS.filter("substring(name,0,1)='M'").show()
 
     //排序
-    studentDF.sort(studentDF("name")).show()
-    studentDF.sort(studentDF("name").desc).show()
-    studentDF.sort("name", "id").show()
-    studentDF.sort(studentDF("name").asc, studentDF("id").desc).show()
+    studentDS.sort(studentDS("name")).show()
+    studentDS.sort(studentDS("name").desc).show()
+    studentDS.sort("name", "id").show()
+    studentDS.sort(studentDS("name").asc, studentDS("id").desc).show()
 
     //重命名
-    studentDF.select(studentDF("name").as("student_name")).show()
+    studentDS.select(studentDS("name").as("student_name")).show()
 
     //join操作
-    val studentDF2 = rdd.map(_.split("\\|")).map(line => Student(line(0).toInt, line(1), line(2), line(3))).toDF()
-    studentDF.join(studentDF2, studentDF.col("id") === studentDF2.col("id")).show()
+    val studentDS2 = rdd.map(_.split("\\|")).map(line => Student(line(0).toInt, line(1), line(2), line(3))).toDF()
+    studentDS.join(studentDS2, studentDS.col("id") === studentDS2.col("id")).show()
 
     spark.stop()
   }
