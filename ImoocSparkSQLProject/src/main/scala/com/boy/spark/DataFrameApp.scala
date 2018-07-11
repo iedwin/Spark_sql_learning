8e826ec1ab8eb58c834d5878a707056e18a3d53d
edwin <edwin@edwins-MacBook-Pro.local>
2018-07-11 22:27:14 +0800

    some changes

--- ImoocSparkSQLProject/src/main/scala/com/boy/spark/DataFrameApp.scala
+++ ImoocSparkSQLProject/src/main/scala/com/boy/spark/DataFrameApp.scala
@@ -1,5 +1,6 @@
 package com.boy.spark
 
+import com.tool.LogConf
 import org.apache.spark.sql.SparkSession
 
 /**
@@ -8,6 +9,8 @@ import org.apache.spark.sql.SparkSession
 object DataFrameApp {
 
   def main(args: Array[String]): Unit = {
+
+    LogConf.setStreamingLogLevels
 
     val spark = SparkSession.builder().appName("DataFrameApp").master("local[2]").getOrCreate()
 
